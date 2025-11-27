dc824054a70e65dcbd663d4db04dd2bd63eb7a99
rafaelajuulia <rafaelajulia674@gmail.com>
2025-11-27 17:59:32 -0300

    Criar Prefab de Tile com número e cor

--- Assets/Scripts/TileBoard.cs
+++ Assets/Scripts/TileBoard.cs
@@ -4,7 +4,42 @@ using UnityEngine;
 public class TileBoard : MonoBehaviour
 {
     public Tile tilePrefab;
+    public TileState[] tileStates;
     private TileGrid grid;
     private List<Tile> tiles;
+    private void Awake()
+    {
+        grid = GetComponentInChildren<TileGrid>(); 
+        tiles = new List<Tile>(16);   
+    }
+
+    private void Start()
+    {
+        CreateTile();
+        CreateTile();
+    }
+
+    private void CreateTile()
+    {
+        Tile tile = Instantiate(tilePrefab, grid.transform);
+        tile.SetState(tileStates[0], 2);
+        tile.Spaw(grid.GetRandomEmptyCell());
+        tiles.Add(tile);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+            
+        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+            
+        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+            
+        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            
+        }
+    }
+
+    
 
 }
--- Assets/Scripts/TileGrid.cs
+++ Assets/Scripts/TileGrid.cs
@@ -1,17 +1,18 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TileGrid : MonoBehaviour
 {
    public TileRow[] rows { get; private set; }
    public TileCell[] cells { get; private set; }
-    public int Size => cells.Length;
-    public int Height => rows.Length;
-    public int Width => Size / Height;
-    private void Awake()
-    {
+   public int size => cells.Length;
+   public int height => rows.Length;
+   public int width => size / height;
+   private void Awake()
+   {
         rows = GetComponentsInChildren<TileRow>();
         cells = GetComponentsInChildren<TileCell>();
-    }
+   }
     
     private void Start()
     {
@@ -23,5 +24,27 @@ public class TileGrid : MonoBehaviour
 
             }
         }
+    }
+
+    public TileCell GetRandomEmptyCell()
+    {
+        int index = Random.Range(0, cells.Length);
+        int startingIndex = index;
+
+        while (cells[index].occupied)
+        {
+            index++;
+
+            if (index >= cells.Length)
+            {
+                index = 0;
+            }
+
+            if (index == startingIndex)
+            {
+                return null;
+            }
+        }
+        return cells[index];
     }
 }
